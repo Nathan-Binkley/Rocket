9a34e4ea1039d24a0ec1138f84bc8369ac742fb6
Nathan-Binkley <nbink2@gmail.com>
2024-07-25 22:19:35 -0400

    DAYTA TRANSFWERRRRR

--- consi/src/app/browse/page.tsx
+++ consi/src/app/browse/page.tsx
@@ -1,61 +1,78 @@
+"use client";
 import Card from "../../components/card";
 import Image from "next/image";
 import Link from "next/link";
-const data = [
-    {
-        id: 1,
-        image: "/img/card-top.jpg",
-        title: "Restaurant Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-        alt: "test",
-        categories: ["category1", "category2", "category3"]
-    },
-    {
-        id: 2,
-        image: "/img/card-top.jpg",
-        title: "Restaurant Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-        alt: "test",
-        categories: ["category1", "category2", "category3"]
-    },
-    {
-        id: 3,
-        image: "/img/card-top.jpg",
-        title: "Restaurant Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-        alt: "test",
-        categories: ["category1", "category2", "category3"]
-    },
-    {
-        id: 4,
-        image: "/img/card-top.jpg",
-        title: "Restaurant Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-        alt: "test",
-        categories: ["category1", "category2", "category3"]
-    },
-    {
-        id: 5,
-        image: "/img/card-top.jpg",
-        title: "Restaurant Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-        alt: "test",
-        categories: ["category1", "category2", "category3"]
-    },
-    {
-        id: 6,
-        image: "/img/card-top.jpg",
-        title: "Restaurant Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-        alt: "test",
-        categories: ["category1", "category2", "category3"]
-    }
-];
+import { useState, useEffect } from 'react';
+import useSWR from 'swr';
+
+// const data = [
+//     {
+//         id: 1,
+//         image: "/img/card-top.jpg",
+//         title: "Restaurant Title",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+//         alt: "test",
+//         categories: ["category1", "category2", "category3"]
+//     },
+//     {
+//         id: 2,
+//         image: "/img/card-top.jpg",
+//         title: "Restaurant Title",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+//         alt: "test",
+//         categories: ["category1", "category2", "category3"]
+//     },
+//     {
+//         id: 3,
+//         image: "/img/card-top.jpg",
+//         title: "Restaurant Title",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+//         alt: "test",
+//         categories: ["category1", "category2", "category3"]
+//     },
+//     {
+//         id: 4,
+//         image: "/img/card-top.jpg",
+//         title: "Restaurant Title",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+//         alt: "test",
+//         categories: ["category1", "category2", "category3"]
+//     },
+//     {
+//         id: 5,
+//         image: "/img/card-top.jpg",
+//         title: "Restaurant Title",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+//         alt: "test",
+//         categories: ["category1", "category2", "category3"]
+//     },
+//     {
+//         id: 6,
+//         image: "/img/card-top.jpg",
+//         title: "Restaurant Title",
+//         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+//         alt: "test",
+//         categories: ["category1", "category2", "category3"]
+//     }
+// ];
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function Page() {
+    const [restaurants, setRestaurants] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const { data, error } = useSWR("http://localhost:8080/api/restaurants-nearby?zipCode=Test Zip Code", fetcher)
+
+
+
+    if (error) return <div>Failed to Load!</div>
+    if (!data) return <div>Loading...</div>
+
     let content = [];
     let card;
     for (let i = 0; i < data.length; i++) {
-        card = <Card key={i} image={data[i].image} title={data[i].title} description={data[i].description} alt={data[i].alt} categories={data[i].categories} />;
+        card = <Card key={data[i].id} image={data[i].image} name={data[i].name} cuisine={data[i].cuisine} alt={data[i].alt} tags={data[i].tags} />;
         content.push(card);
     }
 
--- consi/src/components/card.tsx
+++ consi/src/components/card.tsx
@@ -3,15 +3,15 @@ export default function Card(props) {
         <div className="bg-blue-300 max-w-sm  overflow-hidden shadow-lg px-3 py-3 rounded-s">
             <img className="w-full" src={props.image} alt={props.alt} />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{props.title}</div>
+                <div className="font-bold text-xl mb-2">{props.name}</div>
                 <p className="text-gray-700 text-base">
-                    {props.description}
+                    {props.cuisine}
                 </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.categories[0]}</button>
-                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.categories[1]}</button>
-                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.categories[2]}</button>
+                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.tags[0]}</button>
+                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.tags[1]}</button>
+                <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{props.tags[2]}</button>
             </div>
         </div>
     )
